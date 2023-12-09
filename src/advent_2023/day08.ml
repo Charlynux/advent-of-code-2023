c99284a88ba74c0e1e788b1c769cff03f7ff7adb
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2023-12-09 11:36:30 +0100

    Jour 8 - Partie 2 solution
    Largement optimisable en arrêtant le parcours pour un noeud qui a déjà
    trouvé son "Z".
    
    Le code de gcd et lcm a été honteusement repris de Rosetta...

--- src/advent_2023/day08.ml
+++ src/advent_2023/day08.ml
@@ -66,41 +66,74 @@ module NodeSet = Set.Make(Node);;
 
 let ends_with c node = c = (String.get node 2);;
 
-let get_starting_set map =
+let get_starting_list map =
   (NodeMap.to_list map
    |> List.map fst
-   |> List.filter (ends_with 'A')
-   |> NodeSet.of_list);;
+   |> List.filter (ends_with 'A'));;
+
+let get_starting_set map = get_starting_list map |> NodeSet.of_list;;
+
+let get_starting_array map = get_starting_list map |> Array.of_list;;
+
+let print_status count pos node =
+  print_int count; print_char ',';
+  print_int pos; print_char ',';
+  print_string node; print_char ';';
+  print_endline "";;
+
+let peek print_fn predicate =
+  (fun i node ->
+    let result = (predicate node) in
+    if result then (print_fn i node);
+    result);;
+
+let rec gcd u v =
+  if v <> 0 then (gcd v (u mod v))
+  else (abs u);;
+
+let lcm m n =
+  match m, n with
+  | 0, _ | _, 0 -> 0
+  | m, n -> abs (m * n) / (gcd m n);;
+
 
 let solve_part2 (map : instruction NodeMap.t) (directions : direction list) =
-  let rec loop (ds : direction list) nodes (n : int) =
+  let rec loop (found : (int * int * string) list)
+            (ds : direction list) nodes (n : int) =
     if (List.is_empty ds) then
-      loop directions nodes n
-    else if (NodeSet.for_all
-               (ends_with 'Z')
-               nodes) then
-      n
-    else if (n > 200000) then
-      n
-     else
-      loop
+      loop found directions nodes n
+    else if
+      (let fs = (found
+                 |> List.map (fun (_, pos, _) -> pos)
+                 |> List.sort_uniq compare |> List.length)
+       in fs = Array.length nodes)
+    then
+      (let positions = List.init (Array.length nodes) (fun x -> x) in
+       let hits = positions
+                  |> List.map (fun pos -> List.find (fun (_, p, _) -> p = pos) found)
+                  |> List.map (fun (n, _, _) -> n) in
+      List.fold_left lcm (List.hd hits) (List.tl hits))
+    else
+       loop
+         (found @
+            (nodes
+             |> Array.to_list
+             |> List.mapi (fun pos node -> (n, pos, node))
+             |> List.filter (fun (_, _, node) -> (ends_with 'Z' node))))
         (List.tl ds)
-        (NodeSet.map
+        (Array.map
            (fun n -> (get_node (List.hd ds) (NodeMap.find n map)))
            nodes)
         (n + 1)
   in
   loop
+    []
     directions
-    (get_starting_set map)
+    (get_starting_array map)
     0;;
 
 let (dirs, map) = parse_input (read_lines "../../data/day08-example2.input") in
     solve_part2 map dirs;;
 
-(*
-  let (dirs, map) = parse_input (read_lines "../../data/day08.input") in
-  solve_part2 map dirs;;
-
-
- *)
+let (dirs, map) = parse_input (read_lines "../../data/day08.input") in
+    solve_part2 map dirs;;
