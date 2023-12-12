0be3beb660c39a73a1ca00888cc2c165256985b6
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2023-12-12 20:27:40 +0100

    Jour 12 - Partie 2 KO avec solution naïve

--- src/advent_2023/day12.ml
+++ src/advent_2023/day12.ml
@@ -1,3 +1,8 @@
+#load "str.cma";;
+
+#mod_use "utils.ml";;
+open Utils;;
+
 (*
   3 2 1
 
@@ -46,10 +51,12 @@ calculate_arrangements "????.#...#..." [4;1;1];; (* 1 arrangement *)
 calculate_arrangements "????.######..#####." [1;6;5];; (* 4 arrangements *)
 calculate_arrangements "?###????????" [3;2;1];; (* 10 arrangements *)
 
+let parse_groups groups = groups |> String.split_on_char ',' |> List.map int_of_string;;
+
 let parse_line line =
   let pattern::groups::[] = String.split_on_char ' ' line in
-  (pattern,
-   groups |> String.split_on_char ',' |> List.map int_of_string);;
+  (pattern, parse_groups groups);;
+
 
 parse_line "?###???????? 3,2,1";;
 
@@ -61,3 +68,26 @@ let solve_part1 input =
 
 solve_part1 "../../data/day12-example.input";;
 solve_part1 "../../data/day12.input";;
+
+let expand_pattern (pattern, groups) =
+  let rec loop n result =
+    if (n = 0) then
+      result
+    else
+      loop (n - 1) ((String.cat
+                       (fst result)
+                       (String.cat "?" pattern)),
+                    (snd result)@groups) in
+  loop 4 (pattern, groups);;
+
+expand_pattern ("???.###", [1;1;3]);;
+
+let solve_part2 input =
+  read_lines input
+  |> List.map parse_line
+  |> List.map expand_pattern
+  |> List.map (fun (pattern, groups) -> calculate_arrangements pattern groups)
+  |> list_sum;;
+
+solve_part2 "../../data/day12-example.input";;
+(* solve_part2 "../../data/day12.input";; *)
