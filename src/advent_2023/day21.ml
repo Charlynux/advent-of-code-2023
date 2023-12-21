aa06a9d38edd0599e160a260030b0b61092cf455
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2023-12-21 19:15:38 +0100

    Jour 21 - Partie 1

--- src/advent_2023/day21.ml
+++ src/advent_2023/day21.ml
@@ -42,3 +42,30 @@ let parse_input lines =
 
 parse_input (read_lines "../../data/day21-example.input");;
 parse_input (read_lines "../../data/day21.input");;
+
+let neighbors (x, y) = [(x - 1, y); (x + 1, y);
+                        (x, y - 1); (x, y + 1)];;
+
+let in_bounds (max_x, max_y) (x, y) =
+  (x >= 0 && x < max_x) && (y >= 0 && y < max_y);;
+
+let max_n = 64
+and (start, bounds, rocks) =
+  parse_input (read_lines "../../data/day21.input") in
+    match start with
+      None -> raise Not_found
+    | Some start_point ->
+       let rec loop opens n =
+         if (n > max_n || PointsSet.is_empty opens) then
+           opens
+         else
+           loop
+             (opens
+              |> PointsSet.to_list
+              |> List.concat_map neighbors
+              |> List.filter (in_bounds bounds)
+              |> PointsSet.of_list
+              |> (fun s -> PointsSet.diff s rocks))
+             (n + 1) in
+       loop (PointsSet.singleton start_point) 1
+       |> PointsSet.cardinal;;
